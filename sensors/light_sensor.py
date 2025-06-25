8f3d6ac4eb4b6e780b3de66305d553a6ac63f919
Nagyeong514 <dksskrud1027@gmail.com>
2025-06-25 23:34:38 +0900

    Add modeling assets

--- sensors/light_sensor.py
+++
@@ -1,8 +0,0 @@
-## sensors/light_sensor.py
-
-from gpiozero import DigitalInputDevice
-
-light_sensor = DigitalInputDevice(22)
-
-def read_light():
-    return 1 - int(light_sensor.value)  # ✅ 반전 적용
